a28fdc615a9aa3c42c659d805dda0023eeb1bc0a
yingchen123541 <54613135+yingchen123541@users.noreply.github.com>
2023-05-24 15:12:49 -0700

    logger function to store data signal in sensor board memory

--- examples/dataLabel_yuki.py
+++ examples/dataLabel_yuki.py
@@ -7,23 +7,55 @@ from mbientlab.warble import *
 from time import sleep
 import platform
 import six
+from threading import Event
 
-device = MetaWear("C5:12:30:A0:1D:D8")
-device.connect()
+#pre-defined function:
+#function1: create_voidp function is for logger feature to store data signal in sensor board memory to access it later
+def create_voidp(fn, **kwargs):
+    e = kwargs['event'] if 'event' in kwargs else Event()
 
-print("Connected to " + device.address + " over " + ("USB" if device.usb.is_connected else "??"))
-print("Device information: " + str(device.info))
+    result = [None]
+    def handler(ctx, pointer):
+        result[0] = RuntimeError("Could not create " + (kwargs['resource'] if 'resource' in kwarg else "resource") ) if pointer == None else pointer
+        e.set()
+
+    callback_wrapper = FnVoid_VoidP_VoidP(handler)
+    fn(callback_wrapper)
+    e.wait()
+
+    e.clear()
+    if (result[0] is RuntimeError):
+        raise result[0]
+    return result[0]
+
+
+
+#connect to sensor NO.1 with address(C5:12:30:A0:1D:D8) on the sensor back sticker
+device1 = MetaWear("C5:12:30:A0:1D:D8")
+device1.connect()
+print("Connected to " + device1.address + " over " + ("USB" if device1.usb.is_connected else "??"))
+print("Device1 information: " + str(device1.info))
 sleep(5.0)
 
-#call MetaWear API, blink the LED green in SDK tutorial 
+#code for fully resetting metawear board
+#libmetawear.mbl_mw_debug_reset(d.board)
+
+#MetaWear API, blink the LED green in SDK tutorial 
 pattern= LedPattern(repeat_count= Const.LED_REPEAT_INDEFINITELY)
 libmetawear.mbl_mw_led_load_preset_pattern(byref(pattern), LedPreset.SOLID)
-libmetawear.mbl_mw_led_write_pattern(device.board, byref(pattern), LedColor.BLUE)
-libmetawear.mbl_mw_led_play(device.board)
-sleep(10.0)
-libmetawear.mbl_mw_led_stop_and_clear(device.board)
+libmetawear.mbl_mw_led_write_pattern(device1.board, byref(pattern), LedColor.BLUE)
+libmetawear.mbl_mw_led_play(device1.board)
+sleep(5.0)
+libmetawear.mbl_mw_led_stop_and_clear(device1.board)
 sleep(1.0)
 
-device.disconnect()
+#MetaWear API, get data signals from the accelerometer
+signal = libmetawear.mbl_mw_acc_get_acceleration_data_signal(device1.board)
+#store data signal in sensor board memory. Once the memory is full, old data may be overwritten by new data. 
+logger = create_voidp(lambda fn: libmetawear.mbl_mw_datasignal_log(signal, None, fn), resource = "acc_logger")
+
+
+#disconnect sensor device 1
+device1.disconnect()
 sleep(1.0)
-print("Disconnected") 
+print("device1 disconnected") 
