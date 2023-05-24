4e4becb36f1a132a370e6532c20d2d898d58f36d
yingchen123541 <54613135+yingchen123541@users.noreply.github.com>
2023-05-24 16:21:29 -0700

    sensor started collecting data to store in the sensor memory
    - tried printing out the data point that's collecting, but not sure what the result means..how does 2054062474384 represent a data point?
    - still need to figure out how to produce more than 1 data point, not sure if it's only collecting 1 data point then stop automatically, or it's collecting multiple data points but keep updating the same value so only 1 gets print out
    - never call the mbl_mw_logging_stop but it's still stopping and disconnect the sensor somehow

--- examples/dataLabel_yuki.py
+++ examples/dataLabel_yuki.py
@@ -9,6 +9,9 @@ import platform
 import six
 from threading import Event
 
+# event
+e = Event()
+
 #pre-defined function:
 #function1: create_voidp function is for logger feature to store data signal in sensor board memory to access it later
 def create_voidp(fn, **kwargs):
@@ -16,7 +19,7 @@ def create_voidp(fn, **kwargs):
 
     result = [None]
     def handler(ctx, pointer):
-        result[0] = RuntimeError("Could not create " + (kwargs['resource'] if 'resource' in kwarg else "resource") ) if pointer == None else pointer
+        result[0] = RuntimeError("Could not create " + (kwargs['resource'] if 'resource' in kwargs else "resource") ) if pointer == None else pointer
         e.set()
 
     callback_wrapper = FnVoid_VoidP_VoidP(handler)
@@ -28,6 +31,11 @@ def create_voidp(fn, **kwargs):
         raise result[0]
     return result[0]
 
+#function2:
+def create_voidp_int(fn, **kwargs):
+    e = kwargs['event'] if 'event' in kwargs else Event()
+    result = [None]
+
 
 
 #connect to sensor NO.1 with address(C5:12:30:A0:1D:D8) on the sensor back sticker
@@ -37,8 +45,10 @@ print("Connected to " + device1.address + " over " + ("USB" if device1.usb.is_co
 print("Device1 information: " + str(device1.info))
 sleep(5.0)
 
+
 #code for fully resetting metawear board
 #libmetawear.mbl_mw_debug_reset(d.board)
+
 
 #MetaWear API, blink the LED green in SDK tutorial 
 pattern= LedPattern(repeat_count= Const.LED_REPEAT_INDEFINITELY)
@@ -49,10 +59,53 @@ sleep(5.0)
 libmetawear.mbl_mw_led_stop_and_clear(device1.board)
 sleep(1.0)
 
+
+# #MetaWear API, get data signals from the accelerometer
+# signal = libmetawear.mbl_mw_acc_get_acceleration_data_signal(device1.board)
+# #store data signal in sensor board memory. Once the memory is full, old data may be overwritten by new data. 
+# logger = create_voidp(lambda fn: libmetawear.mbl_mw_datasignal_log(signal, None, fn), resource = "acc_logger", event = e)
+# #create an event that reads the accelerometer data signal every time the timer reaches 1000ms(1s):
+# timer = create_voidp(lambda fn: libmetawear.mbl_mw_timer_create_indefinite(device1.board, 1000, 0, None, fn), resource = "timer", event = e)
+# libmetawear.mbl_mw_event_record_commands(timer)
+# libmetawear.mbl_mw_datasignal_read(signal)
+# print(signal)
+# create_voidp_int(lambda fn: libmetawear.mbl_mw_event_end_record(timer, None, fn), event = e)
+
+# #start logging data
+# libmetawear.mbl_mw_logging_start(device1.board, 0)
+# #start timer
+# libmetawear.mbl_mw_timer_start(timer)
+
+
+
+
 #MetaWear API, get data signals from the accelerometer
 signal = libmetawear.mbl_mw_acc_get_acceleration_data_signal(device1.board)
 #store data signal in sensor board memory. Once the memory is full, old data may be overwritten by new data. 
 logger = create_voidp(lambda fn: libmetawear.mbl_mw_datasignal_log(signal, None, fn), resource = "acc_logger")
+#start logging data
+libmetawear.mbl_mw_logging_start(device1.board, 5)
+#libmetawear.mbl_mw_datasignal_read(signal)
+print(signal)
+
+
+
+
+
+
+ 
+# def logger_ready(self, context, pointer):
+#     self.logger = pointer
+
+# logger_created_fn= FnVoid_VoidP_VoidP(logger_ready)
+# libmetawear.mbl_mw_datasignal_log(signal, None, logger_created_fn)
+
+
+
+
+
+
+
 
 
 #disconnect sensor device 1
