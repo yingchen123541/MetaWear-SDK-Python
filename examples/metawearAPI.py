9fbd11eaa980b5a918c7397b771c0b1a47c57164
yingchen123541 <54613135+yingchen123541@users.noreply.github.com>
2023-06-26 00:19:45 -0700

    try out the trigger event API

--- examples/metawearAPI.py
+++ examples/metawearAPI.py
@@ -24,6 +24,31 @@ def reset(self):
 
 
 
+
+
+#working on this part, understand this code and turn it into a function!!!
+#example on how to trigger an event
+# the recorded commands can be executed when an event is triggered
+#we create an event that reads the temperature data signal every time the timer reaches 1000ms
+signal = libmetawear.mbl_mw_multi_chnl_temp_get_temperature_data_signal(d.board, MetaWearRProChannel.ON_BOARD_THERMISTOR)
+logger = create_voidp(lambda fn: libmetawear.mbl_mw_datasignal_log(signal, None, fn), resource = "temp_logger", event = e)
+
+timer = create_voidp(lambda fn: libmetawear.mbl_mw_timer_create_indefinite(d.board, 1000, 0, None, fn), resource = "timer", event = e)
+libmetawear.mbl_mw_event_record_commands(timer)
+libmetawear.mbl_mw_datasignal_read(signal)
+create_voidp_int(lambda fn: libmetawear.mbl_mw_event_end_record(timer, None, fn), event = e)
+
+libmetawear.mbl_mw_logging_start(d.board, 0)
+libmetawear.mbl_mw_timer_start(timer)
+
+
+
+
+
+
+
+
+
 #call functions
 address = "C5:12:30:A0:1D:D8"
 device1 = MetaWear(address)
